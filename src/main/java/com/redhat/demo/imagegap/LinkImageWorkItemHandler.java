e8840e525777934ba2e3f40b98125d59fed56155
Maggie Hu <mhu@redhat.com>
2016-01-22 00:22:29 -0800

    Updated input & output params, preparedStatements

--- src/main/java/com/redhat/demo/imagegap/LinkImageWorkItemHandler.java
+++ src/main/java/com/redhat/demo/imagegap/LinkImageWorkItemHandler.java
@@ -1,43 +1,79 @@
 package com.redhat.demo.imagegap;
 
 import java.sql.Connection;
-import java.sql.Date;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
-
-import javax.naming.Context;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.kie.api.runtime.process.WorkItem;
 import org.kie.api.runtime.process.WorkItemHandler;
 import org.kie.api.runtime.process.WorkItemManager;
 
+import com.redhat.demos.imagegapanalysis.MovieEpisodeRequest;
+
 public class LinkImageWorkItemHandler implements WorkItemHandler {
 
-	public void executeWorkItem(WorkItem workItem, WorkItemManager workItemManager) {
+	public void executeWorkItem(WorkItem workItem,
+			WorkItemManager workItemManager) {
 		try {
 			System.out.println("***** Linking image in Database *****");
 
-			// TODO: JDBC Inserts - MOVIE_EPISODE_POSTER & MOVIE_EPISODE_REQUEST
+			// Poster
+			Integer posterId = (Integer) workItem.getParameter("posterId");
+			String posterUrl = (String) workItem.getParameter("posterUrl");
+			String posterDescription = (String) workItem
+					.getParameter("posterDescription");
+			String posterTags = (String) workItem.getParameter("posterTags");
+
+			// Request
+			MovieEpisodeRequest movieEpisodeRequest = (MovieEpisodeRequest) workItem
+					.getParameter("MovieEpisodeRequest");
+			String name = movieEpisodeRequest.getName();
+			java.util.Date airDate = movieEpisodeRequest.getAirDate();
+			java.sql.Date airDateSql = new java.sql.Date(airDate.getTime());
+			String releaseYear = movieEpisodeRequest.getReleaseYear();
+
+			Map<String, Object> results = new HashMap<String, Object>();
+
 			try {
 				Connection conn = this.getDBConnection();
 				conn.setAutoCommit(true);
+
+				String insertPosterSQL = "INSERT INTO bpms62.MOVIE_EPISODE_POSTER(posterUrl, posterDescription, posterTags) VALUES(?,?,?,?)";
+				String selectPosterSQL = "SELECT posterId, posterUrl FROM bpms62.MOVIE_EPISODE_POSTER WHERE posterUrl = ?";
+				String insertRequestSQL = "INSERT INTO bpms62.MOVIE_EPISODE_REQUEST(name, airDate , releaseYear, posterId) VALUES(?,?,?,?,?)";
+
+				if (posterId == null && posterUrl != null) {
+					PreparedStatement ps1 = conn
+							.prepareStatement(insertPosterSQL);
+					ps1.setString(0, posterUrl);
+					ps1.setString(1, posterDescription);
+					ps1.setString(2, posterTags);
+					ps1.execute();
+				}
 				
-				PreparedStatement stmt1 = conn
-						.prepareStatement("INSERT INTO MOVIE_EPISODE_REQUEST(posterId, posterUrl, posterDescription, posterTags) VALUES(?,?,?,?)");
-				stmt1.setString(0, workItem.getParameter("posterId").toString());
-				stmt1.setString(1, workItem.getParameter("posterUrl").toString());
-				stmt1.setString(2, workItem.getParameter("posterDescription").toString());
-				stmt1.setString(3, workItem.getParameter("posterTags").toString());
-				stmt1.execute();
+				PreparedStatement ps2 = conn
+						.prepareStatement(selectPosterSQL);
+				ps2.setString(1, posterUrl);
+				ResultSet rs = ps2.executeQuery();
+
+				PreparedStatement ps3 = conn
+						.prepareStatement(insertRequestSQL);
 				
-				PreparedStatement stmt2 = conn
-						.prepareStatement("INSERT INTO MOVIE_EPISODE_POSTER(id, name, airDate , releaseYear) VALUES(?,?,?,?)");
-				stmt2.setString(0, workItem.getParameter("Id").toString());
-				stmt2.setString(1, workItem.getParameter("name").toString());
-				stmt2.setString(2, workItem.getParameter("airDate").toString());
-				stmt2.setString(3, workItem.getParameter("releaseYear").toString());
-				stmt2.execute();
+				while (rs.next()) {
+					int posterIdToLink = rs.getInt("posterId");
+					System.out.println("***** Poster Id to link to Image: " + posterIdToLink);
+					
+					ps3.setString(0, name);
+					ps3.setDate(1, airDateSql);
+					ps3.setString(2, releaseYear);
+					ps3.setInt(3, posterId);
+					ps3.execute();
+
+				}
 
 			} catch (SQLException e) {
 				// TODO Auto-generated catch block
@@ -47,17 +83,21 @@ public class LinkImageWorkItemHandler implements WorkItemHandler {
 				// TODO: handle exception
 			}
 
-			workItemManager.completeWorkItem(workItem.getId(), null);
+			workItemManager.completeWorkItem(workItem.getId(), results);
 
 		} catch (Exception e) {
-			System.out.println("***** Link image in Database aborted for WorkItemID " + workItem.getId());
+			System.out
+					.println("***** Link image in Database aborted for WorkItemID "
+							+ workItem.getId());
 			workItemManager.abortWorkItem(workItem.getId());
 			e.printStackTrace();
 		}
 	}
 
 	public void abortWorkItem(WorkItem workItem, WorkItemManager manager) {
-		System.out.println("***** Link image in Database aborted for WorkItemID " + workItem.getId());
+		System.out
+				.println("***** Link image in Database aborted for WorkItemID "
+						+ workItem.getId());
 
 	}
 
