56bd08f8cceca12dbda5a04048a3d4e2fb0e7013
Maggie Hu <mhu@redhat.com>
2016-01-21 23:19:17 -0800

    added indexes and auto increment to create tables

--- src/main/resources/sql-scripts/tables_create.sql
+++ src/main/resources/sql-scripts/tables_create.sql
@@ -1,17 +1,20 @@
 CREATE TABLE `MOVIE_EPISODE_POSTER` (
-  `posterId` int(11) NOT NULL,
+  `posterId` int(11) NOT NULL AUTO_INCREMENT,
   `posterDescription` varchar(255) DEFAULT NULL,
   `posterTags` varchar(255) DEFAULT NULL,
   `posterUrl` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`posterId`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  PRIMARY KEY (`posterId`),
+  KEY `idx_MOVIE_EPISODE_POSTER_posterId` (`posterId`)
+) ENGINE=InnoDB AUTO_INCREMENT=1006 DEFAULT CHARSET=utf8;
 
 CREATE TABLE `MOVIE_EPISODE_REQUEST` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(45) DEFAULT NULL,
   `airDate` varchar(45) DEFAULT NULL,
   `releaseYear` varchar(45) DEFAULT NULL,
-  `posterUrlId` int(11) DEFAULT NULL,
+  `posterId` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`),
-  CONSTRAINT `posterUrlId` FOREIGN KEY (`id`) REFERENCES `MOVIE_EPISODE_POSTER` (`posterId`) ON DELETE NO ACTION ON UPDATE NO ACTION
+  KEY `idx_MOVIE_EPISODE_REQUEST_id` (`id`),
+  KEY `idx_MOVIE_EPISODE_REQUEST_posterUrlId` (`posterId`),
+  CONSTRAINT `posterId` FOREIGN KEY (`id`) REFERENCES `movie_episode_poster` (`posterId`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
